20c2235ff6a36b9b7aa27eded4ab4ecd78445cdd
DonatienLege <donatien.lege@agroparistech.fr>
2022-01-11 11:32:06 +0000

    correction 0

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from MCTS import MonteCarloTreeSearchNode
 import numpy as np
-
+from matplotlib import pyplot
 
 reward_list_cparam = []
-cparam = []
-for i in range(100):
-    c_param.append(i/10)
+si_no= []
+for i in range(1,21):
+    si_no.append(100*i)
     initial_state = np.array([[9, 9, 9, 9, 9, 9, 9, 9],
                               [9, 0, 0, 0, 0, 0, 0, 9],
                               [9, 0, 0, 0, 0, 0, 0, 9],
@@ -14,10 +14,12 @@ for i in range(100):
                               [9, 0, 0, 0, 0, 0, 0, 9],
                               [9, 0, 0, 0, 0, 0, 0, 9],
                               [9, 9, 9, 9, 9, 9, 9, 9]])
-    root = MonteCarloTreeSearchNode(state = initial_state, c_param=i/10, simulation_no=100)
+    root = MonteCarloTreeSearchNode(state = initial_state, c_param=1.4, simulation_no=200*i)
     selected_node = root.best_action()
+    while not root._results[1]:
+        root = MonteCarloTreeSearchNode(state = initial_state, c_param=1.4, simulation_no=200*i)
+        selected_node = root.best_action()
     reward_list_cparam.append(root._results[1])
-print(reward_list_cparam)
-
-
+pyplot.plot(si_no, reward_list_cparam)
+pyplot.show()
 
